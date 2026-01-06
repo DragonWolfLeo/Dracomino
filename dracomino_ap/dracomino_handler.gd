ee258c480f72ccd1d52e3e84b36c4c7d613d78d0
Emily Venezia <emilygamergirl99@gmail.com>
2026-01-06 18:34:59 -0500

    don't calculate checked locations when GodotAP already does that for you

--- dracomino_ap/dracomino_handler.gd
+++ dracomino_ap/dracomino_handler.gd
@@ -203,11 +203,6 @@ func _on_connected(conn:ConnectionInfo, json:Dictionary):
 	missingPickups.clear()
 	id_to_line.clear()
 	id_to_pickupCoord.clear()
-	
-	var _checked:Dictionary[int,bool] = {}
-	if json.has("checked_locations"):
-		for loc:int in json["checked_locations"]:
-			_checked[loc] = true
 
 	var locsToCollect:Array[int] = []
 	var _missingLocations_changed:bool = false
@@ -215,7 +210,7 @@ func _on_connected(conn:ConnectionInfo, json:Dictionary):
 	for loc_id:int in conn.slot_locations:
 		var loc_data:CONSTANTS.LocationData = CONSTANTS.LOCATIONS.get(loc_id)
 		if loc_data:
-			var checked:bool = _checked.get(loc_id, false)
+			var checked:bool = conn.slot_locations[loc_id]
 			# Sync missingLocations and checkedLocations with server
 			if not checked:
 				if checkedLocations.get(loc_id, false):
@@ -263,7 +258,7 @@ func _on_connected(conn:ConnectionInfo, json:Dictionary):
 	# Set up lookup tables
 	for i:int in range(allLineLocations.size()):
 		id_to_line[allLineLocations[i]] = i
-		missingLines[i] = not _checked.get(allLineLocations[i], false)
+		missingLines[i] = not conn.slot_locations.get(allLineLocations[i], false)
 
 	for k:Vector2i in missingPickupCoordinates:
 		id_to_pickupCoord[missingPickupCoordinates.get(k,Vector2i())] = k
