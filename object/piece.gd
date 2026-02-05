73af7b3886ff3832c6ac46706e067bc2a5619fa8
Dragon Wolf Leo <dleowolf@gmail.com>
2026-02-05 09:27:56 -0600

    Reset gravity timer after soft dropping

--- object/piece.gd
+++ object/piece.gd
@@ -118,6 +118,8 @@ func _physics_process(delta: float) -> void:
 			movementType = MOVEMENT.SOFT_DROP
 			moved = Vector2i.DOWN
 			softDropTimer.start()
+			# Avoid falling too soon
+			gravityTimer.start()
 	
 	if moved != Vector2i.ZERO:
 		movement_requested.emit(self, moved, movementType)
@@ -245,6 +247,8 @@ func _on_SoftDropTimer_timeout():
 		movement_requested.emit(self, Vector2i.DOWN, MOVEMENT.SOFT_DROP)
 		softDropTimer.wait_time = SOFT_DROP_REPEAT_WAIT_TIME
 		softDropTimer.start()
+		# Avoid falling too soon
+		gravityTimer.start()
 	else:
 		softDropTimer.wait_time = SOFT_DROP_WAIT_TIME
 
