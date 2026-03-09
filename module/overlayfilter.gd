22ce4716c7ccc78ad9e871ceb862256d0c2a1454
Dragon Wolf Leo <dleowolf@gmail.com>
2026-03-09 00:17:32 -0500

    Add debug toggles for overlays

---
+++ module/overlayfilter.gd
@@ -0,0 +1,18 @@
+extends ColorRect
+
+@export var debugSignalKey:String = ""
+
+func _ready() -> void:
+	if debugSignalKey.length():
+		hide();
+		SignalBus.getSignal(debugSignalKey+"_enabled").connect(_on_enabledSignal)
+		SignalBus.getSignal(debugSignalKey+"_disabled").connect(_on_disabledSignal)
+
+func _on_enabledSignal():
+	show()
+	var parent:Node = get_parent()
+	if parent:
+		parent.move_child(self, get_child_count(true)-1)
+
+func _on_disabledSignal():
+	hide()
