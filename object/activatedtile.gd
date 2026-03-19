5364dde0cf6db42f9317b913604f8b5972747e6c
Dragon Wolf Leo <dleowolf@gmail.com>
2026-03-19 18:15:02 -0500

    Tween crystal animation

--- object/activatedtile.gd
+++ object/activatedtile.gd
@@ -1,7 +1,16 @@
 extends Sprite2D
 
 @onready var CRYSTALPARTICLE_SCENE:PackedScene = load("res://object/crystalparticle.tscn")
+var ANIMATION_TIME:float = 0.2
+
+func _ready() -> void:
+    # Make glow layer fade in
+    $ActivatedTileGlow.modulate.a = 0
+    var tween := create_tween()
+    tween.tween_property($ActivatedTileGlow, "modulate", Color.WHITE, ANIMATION_TIME).from_current()
+
 func _exit_tree() -> void:
+    # Explode into particles when deleted
     var particles:CPUParticles2D = CRYSTALPARTICLE_SCENE.instantiate()
     get_parent().add_child.call_deferred(particles)
     particles.position = position
