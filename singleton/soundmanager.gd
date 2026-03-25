7c689f91bba04421ef435f637c9eaaacfb09c526
Dragon Wolf Leo <dleowolf@gmail.com>
2026-03-25 10:26:28 -0500

    Add SoundManager singleton

---
+++ singleton/soundmanager.gd
@@ -0,0 +1,13 @@
+extends Node
+
+func play(trackName:String, soundType:String = "sfx"):
+	var targetPlayer = get_node_or_null(trackName) as AudioStreamPlayer
+	var resName = "res://audio/{soundType}/{trackName}.wav".format({soundType=soundType, trackName=trackName})
+	if targetPlayer:
+		targetPlayer.play()
+	elif ResourceLoader.exists(resName):
+		targetPlayer = $default
+		targetPlayer.stream = load(resName)
+		targetPlayer.play()
+	else:
+		printerr("Tried to play invalid %s sound: %s"%[soundType, trackName])
