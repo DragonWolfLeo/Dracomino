1010baf0fb6cab84c95d20f612b4412a68a69796
Dragon Wolf Leo <dleowolf@gmail.com>
2026-03-30 13:23:11 -0500

    Give fish pieces motion

--- level/fishingboard.gd
+++ level/fishingboard.gd
@@ -10,52 +10,52 @@ var fishes:Array[FishPiece] = []
 
 # === Virtuals ===
 func _enter_tree() -> void:
-    var mode:Mode = DracominoUtil.getParentMode(self)
-    if mode:
-        mode.mode_enabled.connect(_on_mode_enabled)
-    else:
-        printerr("FishingBoard._enter_tree warning: does not have mode parent!")
+	var mode:Mode = DracominoUtil.getParentMode(self)
+	if mode:
+		mode.mode_enabled.connect(_on_mode_enabled)
+	else:
+		printerr("FishingBoard._enter_tree warning: does not have mode parent!")
 
 func _input(event: InputEvent) -> void:
-    if event.is_action_pressed("ui_accept") and fishes.size() == 0:
-        spawnFishes()
+	if event.is_action_pressed("ui_accept") and fishes.size() == 0:
+		spawnFishes()
 
 # === Functions ===
 func spawnFish(piece:Piece):
-    if not piece:
-        return
-    for fish in fishes:
-        if fish.piece == piece:
-            return
+	if not piece:
+		return
+	for fish in fishes:
+		if fish.piece == piece:
+			return
 
-    var fishPiece:FishPiece = FISHPIECE_SCENE.instantiate() as FishPiece
-    fishPiece.position = BOUNDS.position + (BOUNDS.size/Vector2(1, fishLimit+1) * Vector2(randf(), fishes.size()+randf()))
-    fishPiece.piece = piece
-    fishPiece.tree_exiting.connect(fishes.erase.bind(fishPiece))
-    fishes.append(fishPiece)
-    add_child(fishPiece)
+	var fishPiece:FishPiece = FISHPIECE_SCENE.instantiate() as FishPiece
+	fishPiece.position = BOUNDS.position + (BOUNDS.size/Vector2(1, fishLimit+1) * Vector2(randf(), fishes.size()+randf()))
+	fishPiece.piece = piece
+	fishPiece.tree_exiting.connect(fishes.erase.bind(fishPiece))
+	fishes.append(fishPiece)
+	add_child(fishPiece)
 
 func spawnFishes():
-    print("spawning fishes")
-    for fish in fishes:
-        fish.queue_free()
-    fishes.clear()
-    board.fillPreview(4)
+	print("spawning fishes")
+	for fish in fishes:
+		fish.queue_free()
+	fishes.clear()
+	board.fillPreview(4)
 
-    if board.previewStorage:
-        for preview:PiecePreview in board.previewStorage.storage:
-            spawnFish(preview.piece)
-            if fishes.size() >= fishLimit:
-                break
-        print("filled pond with %s fishes"%fishes.size())
-        if fishes.size() == 0:
-            quitFishing()
-    else:
-        printerr("FishingBoard.spawnFishes error: board needs previewStorage for this to work!")
+	if board.previewStorage:
+		for preview:PiecePreview in board.previewStorage.storage:
+			spawnFish(preview.piece)
+			if fishes.size() >= fishLimit:
+				break
+		print("filled pond with %s fishes"%fishes.size())
+		if fishes.size() == 0:
+			quitFishing()
+	else:
+		printerr("FishingBoard.spawnFishes error: board needs previewStorage for this to work!")
 
 func quitFishing():
-    pass
+	pass
 
 # === Events ===
 func _on_mode_enabled():
-    spawnFishes()
+	spawnFishes()
--- object/fishpiece.gd
+++ object/fishpiece.gd
@@ -1,19 +1,65 @@
-class_name FishPiece extends TileMapLayer
+class_name FishPiece extends CharacterBody2D
 var piece:Piece:
 	set(value):
 		if piece and piece.tree_exiting.is_connected(queue_free):
-				piece.tree_exiting.disconnect(queue_free)
+			piece.tree_exiting.disconnect(queue_free)
 		piece = value
 		if piece == null:
-				queue_free()
-		renderPiece()
-		piece.makeLimbo()
+			queue_free()
+		renderPiece.call_deferred()
 		piece.tree_exiting.connect(queue_free)
+
+var direction:Vector2 = Vector2.RIGHT
+var speed_min:float = 10
+var speed_max:float = 40
+var speed:float
+var collisionWait_min:float = 0.5
+var collisionWait_max:float = 2
+var collisionWait:float
+var time:float
+var waveFrequency:float = 5
+var waveAmplitude:float = 3
+
+@onready var tileMapLayer:TileMapLayer = $TileMapLayer
+
+# === Virtuals ===
+func _ready() -> void:
+	# Set initial speed and direction
+	direction = Vector2.RIGHT if randf() > 0.5 else Vector2.LEFT
+	speed = randf_range(speed_min, speed_max)
+	collisionWait = randf_range(collisionWait_min, collisionWait_max)
+
+	await get_tree().process_frame
+	# Copy tile collision into the collision body
+	var cells = tileMapLayer.get_used_cells()
+	for cell in cells:
+		var data:TileData = tileMapLayer.get_cell_tile_data(cell) 
+		var points:PackedVector2Array = data.get_collision_polygon_points(0, 0) 
+		var collisionShape = CollisionPolygon2D.new()  
+		collisionShape.polygon = points
+		
+		collisionShape.position = tileMapLayer.map_to_local(cell)
+		add_child(collisionShape)
+
+func _physics_process(delta: float) -> void:
+	time += delta
+	tileMapLayer.position.y = waveAmplitude*sin(time*waveFrequency)
+	var col := move_and_collide(direction*speed*delta)
+	if col:
+		set_physics_process(false)
+		var tween:Tween = create_tween().set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
+		tween.tween_interval(collisionWait)
+		await tween.finished
+		direction *= -1
+		set_physics_process(true)
 
 # === Functions ===
 func renderPiece():
 	if not piece:
 		printerr("FishPiece.renderPiece error: piece is null")
 		return
-	for pos:Vector2i in piece.localCells:
-		set_cell(pos, 0, Vector2i(piece.id, Board.ACTIVE_TILE_ATLAS_ROW))
+	if tileMapLayer:
+		for pos:Vector2i in piece.localCells:
+			tileMapLayer.set_cell(pos, 0, Vector2i(piece.id, Board.ACTIVE_TILE_ATLAS_ROW))
+	else:
+		printerr("FishPiece.renderPiece: tileMapLayer is null for some reason")
