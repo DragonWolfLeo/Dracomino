821e480356d1949ca06a19adf4003455ec154206
Dragon Wolf Leo <dleowolf@gmail.com>
2026-04-01 19:10:10 -0500

    Tweak fishing and clean up magic numbers

--- object/fishing_mini.gd
+++ object/fishing_mini.gd
@@ -19,6 +19,11 @@ signal nothing_caught()
 
 var MIN_STAMINA_STRENGTH:float = 0.2
 var RETRIEVE_DIST_SQ:float = 50*50
+var THROW_VELOCITY:Vector2 = Vector2(150, -150)
+var REEL_ACCELERATION:float = 20
+var REEL_DURATION:float = 3.0
+var CHARGE_DURATION:float = 1.5
+var RESET_DURATION:float = 0.3
 
 var tween:Tween
 var currentCharge:float = 0.0:
@@ -62,14 +67,19 @@ func _process(delta: float) -> void:
 	drawFishingLine()
 
 # === Functions ===
-func reset():
+func reset(immediate:bool = true):
 	if tween: tween.kill()
 	staminaBar.hide()
 	staminaBar.value = 0
-	fishingHook.position = FISHING_HOOK_STARTING_POSITION
 	casted = false
 	currentCharge = 0
 	frame = IDLE_FRAME
+	if immediate:
+		fishingHook.position = FISHING_HOOK_STARTING_POSITION
+	else:
+		tween = create_tween()
+		tween.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUAD)
+		tween.tween_property(fishingHook, "position", FISHING_HOOK_STARTING_POSITION, RESET_DURATION).from_current()
 
 func retrieve():
 	var hooked:FishPiece = fishingHook.hooked
@@ -78,7 +88,7 @@ func retrieve():
 		piece_caught.emit(hooked.piece)
 	else:
 		nothing_caught.emit()
-	reset()
+	reset(false)
 
 func startCharge():
 	if tween: tween.kill()
@@ -86,7 +96,7 @@ func startCharge():
 	casted = false
 	fishingHook.position = fishingRodMarker_charge.position + FISHING_HOOK_STARTING_POSITION - fishingRodMarker_idle.position
 	tween = create_tween()
-	tween.tween_method(setCharge, 0.0, 1.0, 1.0).set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_QUAD)
+	tween.tween_method(setCharge, 0.0, 1.0, CHARGE_DURATION).set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_CUBIC)
 
 func setCharge(value:float):
 	currentCharge = value
@@ -95,10 +105,10 @@ func startReel():
 	if tween: tween.kill()
 	frame = CHARGE_FRAME
 	tween = create_tween()
-	tween.tween_method(reel, 1.0, 0.0, 3.0).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUAD)
+	tween.tween_method(reel, 1.0, 0.0, REEL_DURATION).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUAD)
 
 func reel(amount:float):
-	fishingHook.velocity += fishingHook.position.direction_to(fishingRodMarker_idle.position) * 20 * amount
+	fishingHook.velocity += fishingHook.position.direction_to(fishingRodMarker_idle.position) * REEL_ACCELERATION * amount
 
 func stopReel():
 	if tween: tween.kill()
@@ -116,7 +126,7 @@ func releaseCast():
 		tween = create_tween()
 		tween.tween_interval(0.3)
 		tween.tween_callback(set.bind("frame", IDLE_FRAME))
-		fishingHook.velocity = Vector2(150*currentCharge, -150)
+		fishingHook.velocity = THROW_VELOCITY * Vector2(currentCharge, 1)
 		fishingHook.position = fishingRodMarker_charge.position + FISHING_HOOK_STARTING_POSITION - fishingRodMarker_idle.position
 		casted = true
 	currentCharge = 0
