0c9a82f0f8300e58872d5ac49f0d7dd980e2652f
Dragon Wolf Leo <dleowolf@gmail.com>
2026-04-13 14:56:11 -0500

    Separate outline from activatedtiles

--- module/activatedtile_handler.gd
+++ module/activatedtile_handler.gd
@@ -1,7 +1,7 @@
 class_name ActivatedTileHandler extends Node2D
 
 @onready var activatedTileMap:TileMapLayer = $ActivatedTileMap
-@onready var crystallizedTileMap:TileMapLayer = $CrystallizedTileMap
+@onready var crystallizedTileMap:OutlineTileMap = $CrystallizedTileMap
 
 signal cleared()
 
@@ -16,6 +16,9 @@ static var CRYSTALLIZE_DELAY:float = 0.125
 static var BIG_CRYSTALLIZE_INTERVAL:float = 0.03
 static var BIG_CRYSTALLIZE_DELAY:float = 0.5 - (BIG_CRYSTALLIZE_INTERVAL*10)
 static var FINISH_DELAY:float = 0.6
+static var CRYSTAL_TILE:Vector2i = Vector2i(1,0)
+static var BIGCRYSTAL_TILE:Vector2i = Vector2i(2,0)
+
 # === Functions ===
 func activateChunk(chunk:Board.ClearingChunk, callback:Callable):
 	# Set up shatter tween
@@ -50,83 +53,15 @@ func activateTile(cell:Vector2i) -> void:
 	activatedTileMap.set_cell(cell, 0, Vector2i.ZERO, 1)
 
 func crystallizeTile(cell:Vector2i) -> void:
-	activatedTileMap.set_cell(cell, 1, Vector2i(1,0))
+	activatedTileMap.set_cell(cell, 1, CRYSTAL_TILE)
 
 func bigCrystallizeTile(cell:Vector2i, updateCrystal:bool = true) -> void:
-	var nativeCell:Vector2i = cell*2
-	crystallizedTileMap.set_cell(nativeCell, 0, Vector2i(5,1))
-	# Clear overlapping tiles
-	crystallizedTileMap.set_cell(nativeCell+Vector2i.RIGHT)
-	crystallizedTileMap.set_cell(nativeCell+Vector2i.DOWN)
-	crystallizedTileMap.set_cell(nativeCell+Vector2i.RIGHT+Vector2i.DOWN)
-	if updateCrystal:
-		updateCrystallizedSurrounding(cell)
+	activatedTileMap.set_cell(cell, 1, BIGCRYSTAL_TILE)
+	crystallizedTileMap.setTile(cell, updateCrystal)
 
 func clearBigCrystallizeTile(cell:Vector2i, updateCrystal:bool) -> void:
-	var nativeCell:Vector2i = cell*2
-	crystallizedTileMap.set_cell(nativeCell)
-	if updateCrystal:
-		updateCrystallizedSurrounding(cell)
-
-func updateCrystallizedSurrounding(cell:Vector2i) -> void:
-	for v:Vector2i in [
-		cell,
-		cell+Vector2i.UP,
-		cell+Vector2i.DOWN,
-		cell+Vector2i.LEFT,
-		cell+Vector2i.RIGHT,
-		cell+Vector2i.UP+Vector2i.LEFT,
-		cell+Vector2i.UP+Vector2i.RIGHT,
-		cell+Vector2i.DOWN+Vector2i.LEFT,
-		cell+Vector2i.DOWN+Vector2i.RIGHT,
-	]:
-		updateCrystallizedCell(v)
-
-func updateCrystallizedCell(cell:Vector2i) -> void:
-	if not Board.BOUNDS.has_point(cell) or isCellCrystallized(cell):
-		# Only change crystal edges in bounds
-		return
-	var nativeCell:Vector2i = cell*2
-	var up_occupied = isCellCrystallized(cell+Vector2i.UP)
-	var down_occupied = isCellCrystallized(cell+Vector2i.DOWN)
-	var left_occupied = isCellCrystallized(cell+Vector2i.LEFT)
-	var right_occupied = isCellCrystallized(cell+Vector2i.RIGHT)
-	
-	# Top left
-	crystallizedTileMap.set_cell(nativeCell, 0,
-		Vector2i(2,2) if up_occupied and left_occupied
-		else Vector2i(5,3) if up_occupied
-		else Vector2i(7,1) if left_occupied
-		else Vector2i(7,3) if isCellCrystallized(cell+Vector2i.UP+Vector2i.LEFT)
-		else Vector2i(-1,-1)
-	)
-	# Top right
-	crystallizedTileMap.set_cell(nativeCell + Vector2i.RIGHT, 0,
-		Vector2i(3,2) if up_occupied and right_occupied
-		else Vector2i(6,3) if up_occupied
-		else Vector2i(4,1) if right_occupied
-		else Vector2i(4,3) if isCellCrystallized(cell+Vector2i.UP+Vector2i.RIGHT)
-		else Vector2i(-1,-1)
-	)
-	# Bottom left
-	crystallizedTileMap.set_cell(nativeCell + Vector2i.DOWN, 0,
-		Vector2i(2,3) if down_occupied and left_occupied
-		else Vector2i(5,0) if down_occupied
-		else Vector2i(7,2) if left_occupied
-		else Vector2i(7,0) if isCellCrystallized(cell+Vector2i.DOWN+Vector2i.LEFT)
-		else Vector2i(-1,-1)
-	)
-	# Bottom right
-	crystallizedTileMap.set_cell(nativeCell + Vector2i.DOWN + Vector2i.RIGHT, 0,
-		Vector2i(3,3) if down_occupied and right_occupied
-		else Vector2i(6,0) if down_occupied
-		else Vector2i(4,2) if right_occupied
-		else Vector2i(4,0) if isCellCrystallized(cell+Vector2i.DOWN+Vector2i.RIGHT)
-		else Vector2i(-1,-1)
-	)
-
-func isCellCrystallized(cell:Vector2i) -> bool:
-	return crystallizedTileMap.get_cell_atlas_coords(cell*2) == Vector2i(5, 1)
+	activatedTileMap.set_cell(cell)
+	crystallizedTileMap.clearTile(cell, updateCrystal)
 
 func shatterTile(cell:Vector2i, updateCrystal:bool = true) -> void:
 	activatedTileMap.set_cell(cell)
@@ -154,7 +89,7 @@ func shatterChunkTiles(chunk:Board.ClearingChunk) -> void:
 	Board.mergeCells(cellsToUpdate, Board.getTranslatedCells(cells, Vector2i.UP))
 	Board.mergeCells(cellsToUpdate, Board.getTranslatedCells(cells, Vector2i.DOWN))
 	for cell in cellsToUpdate:
-		updateCrystallizedCell(cell)
+		crystallizedTileMap.updateCell(cell)
 
 func clear() -> void:
 	activatedTileMap.clear()
@@ -184,4 +119,4 @@ func pushDownRows(row:int) -> void:
 				crystallizedTileMap.set_cell(v, 0, crystallizedTileMap.get_cell_atlas_coords(v+CRYSTAL_UP))
 	
 	for x in range(Board.BOUNDS.position.x, Board.BOUNDS.end.x):
-		updateCrystallizedSurrounding(Vector2i(x, row))
+		crystallizedTileMap.updateSurrounding(Vector2i(x, row))
